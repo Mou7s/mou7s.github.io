d9e563ff63593b2883ae0675f187969888219797
mou7s <liuchuge7777777@gmail.com>
2024-01-19 16:20:57 +0800

    Remove unnecessary HTML code

---
+++ app.vue
@@ -0,0 +1,11 @@
+
+
+<template>
+  <div class="grid place-content-center h-screen">
+    <h1 class="text-9xl">{{ date }}</h1>
+  </div>
+</template>
+<script setup>
+const date = new Date()
+</script>
+
