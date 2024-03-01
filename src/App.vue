657e5a7040fc225d738b856b763392c2bd2fd140
mou7s <liuchuge7777777@gmail.com>
2024-03-02 07:12:24 +0800

    Fix HTML doctype and update dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
-        <p class="text-9xl grid place-content-center h-screen dark:bg-neutral-900 dark:text-neutral-50"> {{ date }}
-        </p>
-    </div>
+  <div>
+    <p
+      class="text-7xl grid place-content-center h-screen dark:bg-neutral-900 dark:text-neutral-50"
+    >
+      {{ date }}
+    </p>
+  </div>
 </template>
 
 <script setup>
@@ -11,11 +14,10 @@ import { ref, onUnmounted } from 'vue';
 const date = ref(Date.now());
 
 const intervalId = setInterval(() => {
-    date.value = Date.now();
+  date.value = Date.now();
 }, 1);
 
 onUnmounted(() => {
-    clearInterval(intervalId);
+  clearInterval(intervalId);
 });
 </script>
-
