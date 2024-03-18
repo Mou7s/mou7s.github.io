11fac7cca586657b3a8ac46b9605282296abe3ed
mou7s <liuchuge7777777@gmail.com>
2024-03-18 10:32:09 +0800

    Remove unnecessary code and redirect to Steam profile

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,5 @@
-<template>
-  <div>
-    <p
-      class="text-7xl grid place-content-center h-screen dark:bg-neutral-900 dark:text-neutral-50"
-    >
-      {{ date }}
-    </p>
-  </div>
-</template>
+<template></template>
 
 <script setup>
-import { ref, onUnmounted } from 'vue';
-
-const date = ref(Date.now());
-
-const intervalId = setInterval(() => {
-  date.value = Date.now();
-}, 1);
-
-onUnmounted(() => {
-  clearInterval(intervalId);
-});
+window.location.href = 'https://steamcommunity.com/id/mou7s';
 </script>
